6325d3be9559a3a8383b4c7a362e5307c685034b
Deimar <deimarparn14@gmail.com>
2023-04-11 15:23:16 +0300

    Implemented notifications create endpoint

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -19,3 +19,29 @@ exports.getNotifications = async (req, res) => {
         return res.status(400).send();
     }
 }
+
+exports.createNotifications = async (req, res) => {
+    try {
+        const username = req.params.user;
+        const { content } = req.body;
+
+        const bytes = CryptoJs.AES.decrypt(username, 'secret-key');
+        const decryptedUsername = bytes.toString(CryptoJs.enc.Utf8);
+
+        const userId = await db.query("SELECT id from users WHERE username = ?", [decryptedUsername]);
+        const result = await db.query("INSERT INTO `notifications`(`userId`, `content`) VALUES (?, ?)",
+            [userId[0].id, content]);
+
+        const id = result.insertId;
+
+        if(result.affectedRows) {
+            return res.status(201).json({
+               id: id,
+               content: content
+            })
+        }
+    } catch(error) {
+        console.log(`Error creating notifications: ${error}`);
+        return res.status(400).send();
+    }
+}
