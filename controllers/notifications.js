e96b4b80da60b813e4f5c1e08fc3167ce72e42b5
Deimar <deimarparn14@gmail.com>
2023-04-11 15:28:07 +0300

    Removed unused code

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -4,14 +4,13 @@ const CryptoJs = require('crypto-js');
 exports.getNotifications = async (req, res) => {
     try {
         const username = req.params.user;
-        console.log(username)
+
         const bytes = CryptoJs.AES.decrypt(username, 'secret-key');
         const decryptedUsername = bytes.toString(CryptoJs.enc.Utf8);
 
         const userId = await db.query("SELECT id from users WHERE username = ?", [decryptedUsername]);
         const result = await db.query("SELECT * from notifications WHERE userId = ?", [userId[0].id]);
 
-        const id = result.insertId;
         return res.status(200).send(result);
 
     } catch(error) {
