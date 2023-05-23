7ba6b0c6c25b268f064ac9bd38b4fa6cd75356b4
Deimar <deimarparn14@gmail.com>
2023-05-23 12:48:58 +0300

    Modified register, login endpoint & database

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const db = require ('../models/db');
 
 exports.register = async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { username, password, hex } = req.body;
         const isUserRegistered = await db.query('SELECT username FROM users WHERE username = ?', [username]);
 
         if (!username) {
@@ -17,16 +17,16 @@ exports.register = async (req, res) => {
             res.status(200).json({ message: 'User name available' });
         }
 
-        const result = await db.query('INSERT INTO `users` (`username`, `password`) VALUES (?, ?)',
-            [username, password]);
+        const result = await db.query('INSERT INTO `users` (`username`, `password`,`hex`) VALUES (?, ?, ?)',
+            [username, password, hex]);
 
         if(result.affectedRows) {
             return res.status(201).json({
                 username: username,
-                password: password
+                password: password,
+                hex: hex
             })
         }
-
     } catch(error) {
         console.log(error)
         return res.status(400).send();
@@ -37,9 +37,10 @@ exports.login = async (req, res) => {
     try {
         const { username, password } = req.body;
         const registeredPassword = await db.query('SELECT password FROM users WHERE username = ?', [username]);
+        const colorHex = await db.query('SELECT hex FROM users WHERE username = ?', [username]);
 
         if (registeredPassword[0].password === password) {
-            return  res.status(201).json({auth: true});
+            return  res.status(201).json({auth: true, hex: colorHex});
         } else {
             return res.status(401).json({auth: false});
         }
